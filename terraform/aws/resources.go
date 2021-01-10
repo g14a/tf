c8d9d160327464f5930198e8437202928584602b
Gowtham Munukutla <gowtham.m81197@gmail.com>
2021-01-10 11:35:18 +0530

    Add: boilerplate for EC2 resources

--- terraform/aws/resources.go
+++ terraform/aws/resources.go
@@ -285,6 +285,9 @@ func resources() []string {
 		"aws_lambda_layer_version",
 		"aws_lambda_permission",
 		"aws_lambda_provisioned_concurrency_config",
+		"aws_spot_datafeed_subscription",
+		"aws_spot_fleet_request",
+		"aws_spot_instance_request",
 	}
 }
 
