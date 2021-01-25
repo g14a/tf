8d508817b37eed103f3f3c203e9d0ef9f98819ac
Gowtham Munukutla <gowtham.m81197@gmail.com>
2021-01-25 18:27:37 +0530

    Add: schema changes to aws_ami. lots of WIP

--- terraform/aws/resourceprompts/aws_ec2_prompts.go
+++ terraform/aws/resourceprompts/aws_ec2_prompts.go
@@ -21,58 +21,41 @@ func AWSAMIPrompt() {
 		fmt.Println(err)
 	}
 
-	prompts := map[string]types.TfPrompt{}
-	var promptOrder []string
-
-	prompts["name"] = types.TfPrompt{
-		Label: "Enter name:\n(Required) A region-unique name for the AMI.",
-		Prompt: promptui.Prompt{
-			Label: "",
+	schema := []types.Schema{
+		{
+			Field: "name",
+			Ex:    "",
+			Doc:   "(Required) A region-unique name for the AMI.",
+		},
+		{
+			Field: "description",
+			Ex:    "",
+			Doc:   "(Optional) A longer, human-readable description for the AMI.",
+		},
+		{
+			Field:     "ena_support",
+			Ex:        "(true/false)",
+			Doc:       "(Optional) Specifies whether enhanced networking with ENA is enabled. Defaults to false",
+			Validator: validators.BoolValidator,
+		},
+		{
+			Field: "root_device_name",
+			Ex:    "",
+			Doc:   "(Optional) The name of the root device (for example, /dev/sda1, or /dev/xvda)",
+		},
+		{
+			Type:  "select",
+			Field: "architecture",
+			Doc:   "(Optional) Machine architecture for created instances. Defaults to \"x86_64\".",
+			Items: []string{"i386", "x86_64", "arm64"},
+		},
+		{
+			Field:     "tags",
+			Ex:        "k1=v1,k2=v2",
+			Doc:       "(Optional) A map of tags to assign to the resource.",
+			Validator: validators.RCValidator,
 		},
 	}
-	promptOrder = append(promptOrder, "name")
-
-	prompts["description"] = types.TfPrompt{
-		Label: "Enter description:\n(Optional) A longer, human-readable description for the AMI.",
-		Prompt: promptui.Prompt{
-			Label: "",
-		},
-	}
-	promptOrder = append(promptOrder, "description")
-
-	prompts["ena_support"] = types.TfPrompt{
-		Label: "Enter ena_support(true/false):\n(Optional) Specifies whether enhanced networking with ENA is enabled. Defaults to false",
-		Prompt: promptui.Prompt{
-			Label:    "",
-			Validate: validators.BoolValidator,
-		},
-	}
-	promptOrder = append(promptOrder, "ena_support")
-
-	prompts["root_device_name"] = types.TfPrompt{
-		Label: "Enter root_device_name:\n(Optional) The name of the root device (for example, /dev/sda1, or /dev/xvda",
-		Prompt: promptui.Prompt{
-			Label: "",
-		},
-	}
-	promptOrder = append(promptOrder, "root_device_name")
-
-	prompts["architecture"] = types.TfPrompt{
-		Label: "Enter architecture:\n(Optional) Machine architecture for created instances. Defaults to \"x86_64\".",
-		Prompt: promptui.Prompt{
-			Label: "",
-		},
-	}
-	promptOrder = append(promptOrder, "architecture")
-
-	prompts["tags"] = types.TfPrompt{
-		Label: "Enter tags e.g. k1=v1,k2=v2:\n(Optional) A map of tags to assign to the resource.",
-		Prompt: promptui.Prompt{
-			Label:    "",
-			Validate: validators.RCValidator,
-		},
-	}
-	promptOrder = append(promptOrder, "tags")
 
 	color.Green("\nEnter virtualization_type:\n(Optional) Keyword to choose what virtualization mode created " +
 		"\ninstances will use. Can be either \"paravirtual\" (the default) or \"hvm\". The choice of virtualization " +
@@ -86,40 +69,38 @@ func AWSAMIPrompt() {
 	_, vType, err := vTypePrompt.Run()
 
 	if vType == "paravirtual" {
-		prompts["image_location"] = types.TfPrompt{
-			Label: "Enter image_location:\n(Required) Path to an S3 object containing an image manifest, e.g. created by the ec2-upload-bundle command in the EC2 command line tools.",
-			Prompt: promptui.Prompt{
-				Label: "",
+		schema = append(schema, []types.Schema{
+			{
+				Field: "image_location",
+				Ex:    "",
+				Doc: "(Required) Path to an S3 object containing an image manifest, e.g. " +
+					"\ncreated by the ec2-upload-bundle command in the EC2 command line tools.",
 			},
-		}
-		promptOrder = append(promptOrder, "image_location")
+			{
+				Field: "kernel_id",
+				Ex:    "",
+				Doc: "(Required) The id of the kernel image (AKI) that will be used as the " +
+					"\nparavirtual kernel in created instances.",
+			},
+			{
+				Field: "ramdisk_id",
+				Ex:    "",
+				Doc:   "(Optional) The id of an initrd image (ARI) that will be used when booting the created instances.",
+			},
+		}...)
 
-		prompts["kernel_id"] = types.TfPrompt{
-			Label: "Enter kernel_id:\n(Required) The id of the kernel image (AKI) that will be used as the paravirtual kernel in created instances.",
-			Prompt: promptui.Prompt{
-				Label: "",
-			},
-		}
-		promptOrder = append(promptOrder, "kernel_id")
-
-		prompts["ramdisk_id"] = types.TfPrompt{
-			Label: "Enter ramdisk_id:\n(Optional) The id of an initrd image (ARI) that will be used when booting the created instances.",
-			Prompt: promptui.Prompt{
-				Label: "",
-			},
-		}
-		promptOrder = append(promptOrder, "ramdisk_id")
 	} else if vType == "hvm" {
-		prompts["sriov_net_support"] = types.TfPrompt{
-			Label: "Enter sriov_net_support:\n(Optional) When set to \"simple\" (the default), enables enhanced networking for created instances. No other value is supported at this time.",
-			Prompt: promptui.Prompt{
-				Label: "",
+		schema = append(schema, []types.Schema{
+			{
+				Field: "sriov_net_support",
+				Ex:    "",
+				Doc: "(Optional) When set to \"simple\" (the default), enables enhanced networking " +
+					"\nfor created instances. No other value is supported at this time.",
 			},
-		}
-		promptOrder = append(promptOrder, "sriov_net_support")
+		}...)
 	}
 
-	resourceBlock := builder.PSOrder(promptOrder, nil, prompts, nil)
+	resourceBlock := builder.PSOrder(types.ProvidePS(schema))
 
 	color.Yellow("\nConfigure nested settings like ebs_block_device/ephemeral_block_device etc [y/n]?\n\n", "text")
 
@@ -137,146 +118,111 @@ func AWSAMIPrompt() {
 		return
 	}
 
-	color.Green("\nEnter ebs_block_device:\n(Optional) Nested block describing an EBS block device that should be attached to created instances. " +
-		"\nThe structure of this block is described below.")
-
-	ebsBlockDevicePrompt := map[string]types.TfPrompt{}
-	var nestedPromptOrder []string
-
-	ebsBlockDevicePrompt["device_name"] = types.TfPrompt{
-		Label: "Enter device_name:\n(Required) The path at which the device is exposed to created instances.",
-		Prompt: promptui.Prompt{
-			Label: "",
-		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "device_name")
-
-	ebsBlockDevicePrompt["delete_on_termination"] = types.TfPrompt{
-		Label: "Enter delete_on_termination(true/false):\n(Optional) Boolean controlling whether the EBS volumes created " +
-			"\nto support each created instance will be deleted once that instance is terminated.",
-		Prompt: promptui.Prompt{
-			Label:    "",
-			Validate: validators.BoolValidator,
-		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "delete_on_termination")
+	color.Green("\nEnter ebs_block_device:\n(Optional) Nested block describing an EBS block device " +
+		"\nthat should be attached to created instances." +
+		"\nThe ebs_block_device supports the following arguments:" +
+		"\n1.device_name\n2.delete_on_termination\n3.encrypted\n4.iops\n5.snapshot_id\n6.volume_size\n7.volume_type\n8.kms_key_id")
 
 	color.Yellow("\nYou can specify encrypted or snapshot_id but not both.\n")
 
-	ebsBlockDevicePrompt["encrypted"] = types.TfPrompt{
-		Label: "Enter encrypted(true/false):\n(Optional) Boolean controlling whether the created EBS volumes will be encrypted. Can't be used with snapshot_id",
-		Prompt: promptui.Prompt{
-			Label:    "",
-			Validate: validators.BoolValidator,
+	ebsBlockDeviceSchema := []types.Schema{
+		{
+			Field: "device_name",
+			Ex:    "",
+			Doc:   "(Required) The path at which the device is exposed to created instances.",
 		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "encrypted")
-
-	ebsBlockDevicePrompt["snapshot_id"] = types.TfPrompt{
-		Label: "Enter snapshot_id:\n(Optional) The id of an EBS snapshot that will be used to initialize the created EBS volumes. " +
-			"\nIf set, the volume_size attribute must be at least as large as the referenced snapshot.",
-		Prompt: promptui.Prompt{
-			Label: "",
+		{
+			Field: "delete_on_termination",
+			Ex:    "(true/false)",
+			Doc: "(Optional) Boolean controlling whether the EBS volumes created to support each created " +
+				"\ninstance will be deleted once that instance is terminated.",
+			Validator: validators.BoolValidator,
 		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "snapshot_id")
-
-	ebsBlockDevicePrompt["iops"] = types.TfPrompt{
-		Label: "Enter iops:\n(Required only when volume_type is \"io1/io2\") Number of I/O operations per second the created volumes will support.",
-		Prompt: promptui.Prompt{
-			Label: "",
+		{
+			Field: "encrypted",
+			Ex:    "(true/false)",
+			Doc: "(Optional) Boolean controlling whether the created EBS volumes will be encrypted. " +
+				"\nCan't be used with snapshot_id.",
+			Validator: validators.BoolValidator,
 		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "iops")
-
-	ebsBlockDevicePrompt["volume_size"] = types.TfPrompt{
-		Label: "Enter volume_size:\n(Required unless snapshot_id is set) The size of created volumes in GiB. If snapshot_id is set and " +
-			"\nvolume_size is omitted then the volume will have the same size as the selected snapshot.",
-		Prompt: promptui.Prompt{
-			Label: "",
+		{
+			Field: "iops",
+			Ex:    "",
+			Doc: "(Required only when volume_type is io1 or io2) Number of I/O operations per second " +
+				"\nthe created volumes will support.",
 		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "volume_size")
-
-	ebsBlockDevicePrompt["kms_key_id"] = types.TfPrompt{
-		Label: "Enter kms_key_id:\n(Optional) The full ARN of the AWS Key Management Service (AWS KMS) CMK to use when encrypting the " +
-			"\nsnapshots of an image during a copy operation. This parameter is only required if you want to use a non-default CMK; " +
-			"\nif this parameter is not specified, the default CMK for EBS is used",
-		Prompt: promptui.Prompt{
-			Label: "",
+		{
+			Field: "snapshot_id",
+			Ex:    "",
+			Doc: "(Optional) The id of an EBS snapshot that will be used to initialize the created " +
+				"\nEBS volumes. If set, the volume_size attribute must be at least as large as the referenced snapshot.",
 		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "kms_key_id")
-
-	volumeTypeSelect := map[string]types.TfSelect{}
-	var nestedSelectOrder []string
-
-	volumeTypeSelect["volume_type"] = types.TfSelect{
-		Label: "(Optional) The type of EBS volume to create. Defaults to \"standard\"",
-		Select: promptui.Select{
-			Label: "",
+		{
+			Field: "volume_size",
+			Ex:    "",
+			Doc: "(Required unless snapshot_id is set) The size of created volumes in GiB. " +
+				"\nIf snapshot_id is set and volume_size is omitted then the volume will have " +
+				"\nthe same size as the selected snapshot.",
+		},
+		{
+			Field: "volume_type",
+			Doc:   "(Optional) The type of EBS volume to create. Defaults to standard",
 			Items: []string{"standard", "io1", "io2", "gp2"},
 		},
+		{
+			Field: "kms_key_id",
+			Ex:    "",
+			Doc: "(Optional) The full ARN of the AWS Key Management Service (AWS KMS) " +
+				"\nCMK to use when encrypting the snapshots of an image during a copy operation. " +
+				"\nThis parameter is only required if you want to use a non-default CMK; " +
+				"\nif this parameter is not specified, the default CMK for EBS is used",
+		},
 	}
-	nestedSelectOrder = append(nestedSelectOrder, "volume_type")
 
-	resourceBlock["ebs_block_device"] = builder.PSOrder(nestedPromptOrder, nestedSelectOrder, ebsBlockDevicePrompt, volumeTypeSelect)
+	resourceBlock["ebs_block_device"] = builder.PSOrder(types.ProvidePS(ebsBlockDeviceSchema))
 
 	color.Green("\nEnter ephemeral_block_device:\n(Optional) Nested block describing an ephemeral block device that should be attached to created instances." +
 		"\nThe ephemeral_block_device supports the following structure:" +
 		"\n1.device_name\n2.virtual_name\n")
 
-	ephemeralBlockDevicePrompt := map[string]types.TfPrompt{}
-
-	ephemeralBlockDevicePrompt["device_name"] = types.TfPrompt{
-		Label: "Enter device_name:\n(Required) The path at which the device is exposed to created instances.",
-		Prompt: promptui.Prompt{
-			Label: "",
+	ephemeralBlockDeviceSchema := []types.Schema{
+		{
+			Field: "device_name",
+			Ex:    "",
+			Doc:   "(Required) The path at which the device is exposed to created instances.",
+		},
+		{
+			Field: "virtual_name",
+			Ex:    "",
+			Doc:   "(Required) A name for the ephemeral device, of the form \"ephemeralN\" where N is a volume number starting from zero.",
 		},
 	}
-	nestedPromptOrder = append(nestedPromptOrder, "device_name")
 
-	ephemeralBlockDevicePrompt["virtual_name"] = types.TfPrompt{
-		Label: "Enter virtual_name:\n(Required) A name for the ephemeral device, of the form \"ephemeralN\" where N is a volume number starting from zero.",
-		Prompt: promptui.Prompt{
-			Label: "",
-		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "virtual_name")
-
-	resourceBlock["ephemeral_block_device"] = builder.PSOrder(nestedPromptOrder[len(nestedPromptOrder)-2:], nil, ephemeralBlockDevicePrompt, nil)
+	resourceBlock["ephemeral_block_device"] = builder.PSOrder(types.ProvidePS(ephemeralBlockDeviceSchema))
 
 	color.Green("\nEnter timeouts block:\n" +
 		"The timeout block supports the following arguments:" +
 		"\n1.create\n2.delete\n3.update")
 
-	timeoutsPrompt := map[string]types.TfPrompt{}
-
-	timeoutsPrompt["create"] = types.TfPrompt{
-		Label: "Enter create: e.g. 40m\n(Defaults to 40 mins) Used when creating the AMI",
-		Prompt: promptui.Prompt{
-			Label: "",
+	timeoutSchema := []types.Schema{
+		{
+			Field: "create",
+			Ex:    "60s | 10m | 2h",
+			Doc:   "Used for creating the AMI",
+		},
+		{
+			Field: "update",
+			Ex:    "60s | 10m | 2h",
+			Doc:   "Used for updating the AMI",
+		},
+		{
+			Field: "delete",
+			Ex:    "60s | 10m | 2h",
+			Doc:   "Used for deregistering the AMI",
 		},
 	}
-	nestedPromptOrder = append(nestedPromptOrder, "create")
 
-	timeoutsPrompt["update"] = types.TfPrompt{
-		Label: "Enter update: e.g. 40m\n(Defaults to 40 mins) Used when updating the AMI",
-		Prompt: promptui.Prompt{
-			Label: "",
-		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "update")
-
-	timeoutsPrompt["delete"] = types.TfPrompt{
-		Label: "Enter delete: e.g. 40m\n(Defaults to 90 mins) Used when deregistering the AMI",
-		Prompt: promptui.Prompt{
-			Label: "",
-		},
-	}
-	nestedPromptOrder = append(nestedPromptOrder, "delete")
-
-	resourceBlock["timeouts"] = builder.PSOrder(nestedPromptOrder[len(nestedPromptOrder)-3:], nil, timeoutsPrompt, nil)
+	resourceBlock["timeouts"] = builder.PSOrder(types.ProvidePS(timeoutSchema))
 
 	builder.ResourceBuilder("aws_ami", blockName, resourceBlock)
 
